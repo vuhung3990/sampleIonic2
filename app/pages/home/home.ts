63301ef694fea09bddc0da3a89d554905f35b0a4
Hung Vu <vuhung3990@gmail.com>
2016-07-03 20:56:29 +0700

    first commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Trang2Page} from "../trang-2/trang-2";
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor(private navController: NavController) {
+
+  }
+
+  /** chuyen sang 1 trang moi */
+  nextPage(){
+      console.log("clicked");
+      this.navController.push(Trang2Page);
+  }
+}
---
+++ app/pages/trang-2/trang-2.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Trang2Page page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/trang-2/trang-2.html',
+})
+export class Trang2Page {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
